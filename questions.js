3a88db6444a4789d9f33a05c7258bcf12d50d81b
Jack Sorensen <jack.sorensen11@gmail.com>
2019-12-06 22:01:04 -0800

    first major push. quiz questions and answers added

---
+++ questions.js
@@ -0,0 +1,43 @@
+var quizBox= document.getElementById("quiz");
+var resultsBox = document.getElementById("results");
+var submitBtn = document.getElementById("submit")
+
+function startQuiz(){}
+
+function showScores(){};
+
+
+var questions = [
+    {
+      title: "What is CSS used for?",
+      choices: ["Creating Sweet Skateboards", "Colors, Shapes, and Styles", "Computation Server Status", "numbers"],
+      answer: "Colors, Shapes, and Styles"
+    },
+    {
+      title: "In Javascript, If, While, and For are considered...",
+      choices: ["quotes", "statements", "functions", "event handlers"],
+      answer: "statements"
+    },
+    {
+        title: "Without using any custom fonts or sizes, which of these would have the biggest text?",
+        choices: ["Body", "Footer", "H3", "H2"],
+        answer: "H2"
+      },
+      {
+        title: "A variable with [] after it would be called a/an",
+        choices: ["array", "event handler", "Jquery", "div"],
+        answer: "array"
+      },
+      {
+        title: "Which of these can be called upon more than once in CSS?",
+        choices: ["class", "ID", "Handle", "Burger King's Chicken Fries"],
+        answer: "class"
+      },
+  ];
+
+  for(i = 0; i < choices.length; i++){
+      choices[i]
+  };
+
+  document.getElementById("startquiz").addEventListener("click",startQuiz(){
+    ;
